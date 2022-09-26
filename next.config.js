8050e2c06bb747a4e3f5e06674e8354327b51cd9
Ding Yi <dingyimac@gmail.com>
2022-09-26 22:53:04 +0800

    2022.0.03
    diable contentlayer config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const { withContentlayer } = require("next-contentlayer")
+//const { withContentlayer } = require("next-contentlayer")
 
 /**
  * @type {import('next').NextConfig}
@@ -41,4 +41,6 @@ const nextConfig = {
   },
 }
 
-module.exports = withContentlayer(nextConfig)
+//module.exports = withContentlayer(nextConfig)
+
+module.exports = nextConfig
