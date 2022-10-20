41f7eeb8ff1477c39a858bc96147e26ad553cefa
Ding Yi <dingyimac@gmail.com>
2022-10-21 01:11:43 +0800

    add preload for local fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,10 +25,15 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           {CssBaseline.flush()}
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Albert+Sans:wght@300;400;500;600&display=swap" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,1,0&display=optional" />
+          <link
+            rel="preload"
+            href="/fonts/Unbounded-Variable.woff2"
+            as="font"
+            type="font/woff"
+            crossOrigin=""
+          />
         </Head>
         <body>
           <Main />
